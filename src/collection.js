abb1562bbc9855ada0b618317e2a5e109761fb8a
jdlrobson <jdlrobson@gmail.com>
2018-01-10 12:49:21 -0800

    Purge after a day
    I think we can cope with this. Let's not purge after 40 minutes..

--- src/collection.js
+++ src/collection.js
@@ -3,12 +3,14 @@ import WikiSocketCollection from 'wikitrender'
 const project = process.env.PROJECT || 'wikipedia';
 const collections = {};
 
+const ONE_DAY = ( 60 * 24 ) * 24;
+
 collections.en = new WikiSocketCollection( {
   id: 'mysocket',
   project: 'en.' + project + '.org',
-  minPurgeTime: 40,
-  maxLifespan: ( 60 * 24 ) * 24,
-  maxInactivity: ( 60 * 24 ) * 24,
+  minPurgeTime: ONE_DAY,
+  maxLifespan: ONE_DAY,
+  maxInactivity: ONE_DAY,
   minSpeed: 0.05
 } );
 collections.sv = new WikiSocketCollection( {
