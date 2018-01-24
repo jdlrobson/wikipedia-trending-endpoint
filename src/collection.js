10207da90d532ffce0be6d9e7a81ef5f3fd72913
jdlrobson <jdlrobson@gmail.com>
2018-01-24 15:33:59 -0800

    Tweak collection parameters

--- src/collection.js
+++ src/collection.js
@@ -9,10 +9,14 @@ const ONE_DAY = ONE_HOUR * 24;
 collections.en = new WikiSocketCollection( {
   id: 'mysocket',
   project: 'en.' + project + '.org',
-  minPurgeTime: ONE_HOUR * 12,
+  minPurgeTime: ONE_HOUR * 3,
   maxLifespan: ONE_DAY,
   maxInactivity: ONE_DAY,
-  minSpeed: 0.05
+  // if something hits 50 edits in 0.5 day we want to keep it.
+  // We want to discard anything with less than 8 edits in 3 hours (0.04(3*60) = 7.2)
+  // 50 edits / (6*60) = 50/360 = 0.138
+  // 50 edits / (24*60) = 34 /1140 = 0.04 edits per minute
+  minSpeed: 0.04
 } );
 collections.sv = new WikiSocketCollection( {
   id: 'sv-socket',
